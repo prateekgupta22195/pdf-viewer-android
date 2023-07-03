93f9e6c2d9ab2389b561e2fa4632fdc623a1dc5b
Prateek Gupta <prateekgupta@Prateek-12624.local>
2023-07-04 00:42:39 +0530

    canvas changes

--- app/src/main/java/com/pg/pdfannotator/PDFPageComposable.kt
+++ app/src/main/java/com/pg/pdfannotator/PDFPageComposable.kt
@@ -13,16 +13,19 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.text.ExperimentalTextApi
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 
+@OptIn(ExperimentalTextApi::class)
 @Composable
 fun PDFPageComposable(index: Int, pdfRenderer: PdfRenderer, scale: Float) {
 
     var bitmap by remember {
         mutableStateOf<Bitmap?>(null)
     }
+
 
     LaunchedEffect(Unit, block = {
         runBlocking(Dispatchers.Default + CoroutineExceptionHandler { _, exception ->
@@ -42,11 +45,49 @@ fun PDFPageComposable(index: Int, pdfRenderer: PdfRenderer, scale: Float) {
         }
 
     })
-    if (bitmap != null) Canvas(modifier = Modifier, onDraw = {
-        scale(scale, scale) {
-            drawImage(bitmap!!.asImageBitmap())
+
+    if (bitmap != null)
+        Canvas(modifier = Modifier) {
+            scale(scale, scale) {
+                drawImage(bitmap!!.asImageBitmap())
+//                drawIntoCanvas { canvas ->
+//                    val text = "Hello, World!"
+//                    val textSize = 48f
+//                    val textColor = Color.White
+//                    val textPaint = Paint().apply {
+//                        isAntiAlias = true
+//                        color = android.graphics.Color.parseColor("#00ffff")
+//                    }
+//                    canvas.nativeCanvas.drawText(text, 100f, 100f, textPaint)
+//                }
+            }
+
         }
-    })
 
-
+//    if (bitmap != null)
+//        Spacer(
+//            modifier = Modifier
+//                .drawWithCache {
+//
+//                    val measuredText =
+//                        textMeasurer.measure(
+//                            AnnotatedString("hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohello"),
+//                            constraints = Constraints.fixed(
+//                                width = (size.width / 3f).toInt(),
+//                                height = (size.height / 3f).toInt()
+//                            ),
+//                            overflow = TextOverflow.Ellipsis,
+//                            style = TextStyle(fontSize = 18.sp, color = Color.Red)
+//                        )
+//
+//
+//
+//                    onDrawBehind {
+//                        scale(scale, scale) {
+//                            drawImage(bitmap!!.asImageBitmap())
+//                            drawText(measuredText)
+//                        }
+//                    }
+//                }
+//        )
 }
